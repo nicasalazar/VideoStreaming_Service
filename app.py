78635a71c64d3c2358ed3c7a1d8dbe5111d8c02e
Rahul <i.am.rahul@live.com>
2022-10-13 21:51:55 -0700

    Added the login page, don't have a real redirect right now

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, redirect,session
 
 app = Flask(__name__)
+app.secret_key = 'SuperSecret'
 
+user = {"username": "admin", "password": "password"}
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')     
+        if username == user['username'] and password == user['password']:
+            
+            session['user'] = username
+            return redirect('/dashboard')
+
+        return "<h1>Wrong username or password</h1>"    #if the username or password does not matches 
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard(): 
+    if('user' in session and session['user'] == user['username']):
+        return '<h1>Video Dashboard</h1>'
+    #here we are checking whether the user is logged in or not
+
+    return '<h1>You are not logged in.</h1>'  #if the user is not in the session
 
 
 if __name__ == '__main__':
